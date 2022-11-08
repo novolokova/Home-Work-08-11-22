de29eaa6dfacc6410d86d86f76c9acc994d50c74
Margarita <m.novolokova@gmail.com>
2022-11-08 12:19:47 +0200

    Task 2

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ if (namber >= 0) {
   console.log("Negative");
 }
 
+// 2) Є дві змінні - num1 і num2. Якщо num1>num2 -
+// вивести результат віднімання, інакше - вивести результат множення
+
+let num1;
+let num2;
+let result;
+if (num1 > num2) {
+    result = num1 - num2;
+    console.log(result);
+  } else {
+    result = num1 * num2;
+    console.log(result);
+  }
