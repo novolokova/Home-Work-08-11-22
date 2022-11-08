9269c8bc20b8ac54e4e331d36a18e5fada6854a2
Margarita <m.novolokova@gmail.com>
2022-11-08 12:13:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1) Якщо значення змінної number позитивне або 0 
+//- виводимо ''positive", інакше - "negative"
+const namber = 0;
+
+if (namber >= 0) {
+  console.log("Positive");
+} else {
+  console.log("Negative");
+}
+
