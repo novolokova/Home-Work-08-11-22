5b542ee81a5a5354b689622e2c2cf4c3dba6c73f
Margarita <m.novolokova@gmail.com>
2022-11-08 12:29:08 +0200

    Task 3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1) Якщо значення змінної number позитивне або 0 
+// 1) Якщо значення змінної number позитивне або 0
 //- виводимо ''positive", інакше - "negative"
 const namber = 0;
 
@@ -14,10 +14,22 @@ if (namber >= 0) {
 let num1;
 let num2;
 let result;
+
 if (num1 > num2) {
-    result = num1 - num2;
-    console.log(result);
-  } else {
-    result = num1 * num2;
-    console.log(result);
-  }
+  result = num1 - num2;
+  console.log(result);
+} else {
+  result = num1 * num2;
+  console.log(result);
+}
+
+//   3) Якщо значення змінної кратно(тобто ділеться без остачі)
+//на 7 і одночасно кратна 3 - виводимо "you win!", інакше - "you loose"
+
+let number = 3;
+
+if (number % 7 === 0 || number % 3 === 0) {
+  console.log("you win!");
+} else {
+  console.log("you loose");
+}
